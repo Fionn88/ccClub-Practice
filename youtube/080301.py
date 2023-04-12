0e49d8200ab34341cd00ddbaeb8cd63248191ac3
Yuan-Heng,Kuo <56747201+Fionn88@users.noreply.github.com>
2023-04-12 17:16:48 +0800

    Day 10

--- youtube/080301.py
+++ youtube/080301.py
@@ -28,3 +28,15 @@ False
 Source
 ccClub Judge
 """
+n = int(input())
+count = 0
+i = 1
+while i < n+1:
+  if n % i == 0:
+    count+=1
+  i += 1
+   
+if count > 2:
+  print(False)
+else:
+  print(True)

f95d1b2f8679d6edabd9e61c362932fdb92730d0
FionnKuo <>
2023-04-14 00:54:51 +0800

    Day 12

--- youtube/08all01.py
+++ youtube/08all01.py
@@ -28,3 +28,17 @@ True
 Source
 ccClub Judge
 """
+
+# 輸入input 轉成 int 
+# 條件1. 判斷數列長度，1 就為 True
+# 條件2. l[i] < l [i+1] 
+
+nums = [int(num) for num in input().split()]
+counter = 0
+if len(nums) == 1:
+  print(True)
+else:
+  for i in range(len(nums) -1 ):
+    if nums[i+1] > nums[i]:
+      counter += 1
+  print(counter == len(nums) -1)
--- youtube/08all02.py
+++ youtube/08all02.py
@@ -30,3 +30,13 @@ Sample Output 2
 Source
 ccClub Judge
 """
+nums = [int(num) for num in input().split()]
+
+counter = 0
+for i in range(0, len(nums)-2):
+  for j in range(i+1, len(nums)-1):
+    for k in range(j+1, len(nums)):
+      if nums[i] + nums[j] == nums[k]:
+        counter += 1
+          
+print(counter)

7dc4ca1ecb45da20e69de4ceab874dff71888c12
Yuan-Heng,Kuo <56747201+Fionn88@users.noreply.github.com>
2023-04-14 14:33:40 +0800

    Day 12

--- youtube/090101.py
+++ youtube/090101.py
@@ -39,7 +39,6 @@ for i in range(len(s)):
     admissions_num = int(s[i].split(':')[1].split('/')[1])
     fee = s[i].split(':')[1].split('/')[2]
     schedule = str(round(sign_up_num / admissions_num * 100)) + '%'
-
     course[s[i].split()[0]] = [sign_up_num, admissions_num, schedule, fee]
     
 print(course)

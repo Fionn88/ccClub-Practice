369c58c262457764b4b0b7e94f0ee773e8b3a846
Yuan-Heng,Kuo <56747201+Fionn88@users.noreply.github.com>
2023-04-14 14:32:57 +0800

    Day 13

--- youtube/090101.py
+++ youtube/090101.py
@@ -31,10 +31,16 @@ s = ['A課程 報名人數/招生人數/費用:60/100/600', 'B課程 報名人�
 course = {}
 
 for i in range(len(s)):
-    sign_up_num = s[i].split(':')[1].split('/')[0]
-    admissions_num = s[i].split(':')[1].split('/')[1]
-    fee = s[i].split(':')[1].split('/')[3]
-    schedule = round(sign_up_num / admissions_num * 100) + '%'
+#     sign_up_num = s[i].split(':')[1].split('/')[0]
+#     admissions_num = s[i].split(':')[1].split('/')[1]
+#     fee = s[i].split(':')[1].split('/')[3]
+#     schedule = round(sign_up_num / admissions_num * 100) + '%'
+    sign_up_num = int(s[i].split(':')[1].split('/')[0])
+    admissions_num = int(s[i].split(':')[1].split('/')[1])
+    fee = s[i].split(':')[1].split('/')[2]
+    schedule = str(round(sign_up_num / admissions_num * 100)) + '%'
+
     course[s[i].split()[0]] = [sign_up_num, admissions_num, schedule, fee]
     
 print(course)
+

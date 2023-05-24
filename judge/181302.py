ea9e8a0da02d6f42d8e3ad44ff93bdda33a3b925
FionnKuo <>
2023-05-24 20:37:51 +0800

    Day 52

---
+++ judge/181302.py
@@ -0,0 +1,43 @@
+"""
+日期計算
+描述
+
+給定一個日期以及一個整數n，請計算從這個日期往後n天的日期。n可能為正也可能為負。
+
+
+輸入
+輸入為兩行，第一行包含一個日期，年月日以空白為間隔；第二行則是包含一個整數n
+
+
+輸出
+輸出為一行，包含一個日期，格式為 YYYY-MM-DD
+
+
+輸入範例 1 
+2020 1 23
+7
+
+輸出範例 1
+2020-01-30
+
+輸入範例 2 
+1911 3 12
+-51
+
+輸出範例 2
+1911-01-20
+
+提示
+建議使用 datetime 這個模組
+
+來源
+ccClub Judge
+"""
+from datetime import datetime, timedelta
+
+date_str = input()
+n = int(input())
+date = datetime.strptime(date_str, '%Y %m %d')
+new_date = date + timedelta(days=n)
+formatted_date = new_date.strftime('%Y-%m-%d')
+print(formatted_date)

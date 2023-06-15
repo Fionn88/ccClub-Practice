d365b713839641da73b1d6ba61e194feaf7a251f
FionnKuo <>
2023-06-16 01:25:27 +0800

    Day 87

--- Web_Scarping/selenuium.py
+++ Web_Scarping/selenuium.py
@@ -23,13 +23,13 @@ this_month = driver.find_element(By.XPATH, '//*[@id="new_search_form"]/div/div/d
 this_month.click()
 time.sleep(2)
 while True:
-    info = driver.find_elements(By.CLASS_NAME,"fa.fa-angle-right")
+    info = driver.find_elements(By.CLASS_NAME,"pull-right")
     for i in range(len(info)):
-# =========== Can not Do Second Time, Debug ..... ================
-        info[i].click()
-        time.sleep(1)
-        print(f"*{driver.find_element(By.CLASS_NAME,'header-title').text}*")
-        driver.execute_script("window.history.go(-1)")
-        # time.sleep(1)
-        # driver.refresh()
-        # time.sleep(1)
+            info = driver.find_elements(By.CLASS_NAME,"pull-right")
+            info[i].click()
+            time.sleep(1)
+            print(f"*{driver.find_element(By.CLASS_NAME,'header-title').text}*")
+            
+            timezone = driver.find_elements(By.CLASS_NAME,"timezoneSuffix")
+            driver.execute_script("window.history.go(-1)")
+            time.sleep(1)

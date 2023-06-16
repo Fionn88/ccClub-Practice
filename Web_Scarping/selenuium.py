f2627bd28a3f884fa92372007e3e7ff64e63053e
FionnKuo <>
2023-06-17 01:11:48 +0800

    Day 87

--- Web_Scarping/selenuium.py
+++ Web_Scarping/selenuium.py
@@ -22,14 +22,31 @@ time.sleep(2)
 this_month = driver.find_element(By.XPATH, '//*[@id="new_search_form"]/div/div/div[1]/div[2]/div[1]/ul/li[1]/ul/li[2]')
 this_month.click()
 time.sleep(2)
+
 while True:
-    info = driver.find_elements(By.CLASS_NAME,"pull-right")
+    info = driver.find_elements(By.CLASS_NAME,"fa-angle-right")
+    print(info)
+# =========== Index Error, Debug ..... ================
     for i in range(len(info)):
-            info = driver.find_elements(By.CLASS_NAME,"pull-right")
-            info[i].click()
-            time.sleep(1)
-            print(f"*{driver.find_element(By.CLASS_NAME,'header-title').text}*")
+        info = driver.find_elements(By.CLASS_NAME,"fa-angle-right")
+        print(info)
+        # Error Happened
+        info[i].click()
+        time.sleep(1)
+        print(f"*{driver.find_element(By.CLASS_NAME,'header-title').text}*")
             
-            timezone = driver.find_elements(By.CLASS_NAME,"timezoneSuffix")
-            driver.execute_script("window.history.go(-1)")
-            time.sleep(1)
+        timezone = driver.find_elements(By.CLASS_NAME,"timezoneSuffix")
+        start = timezone[0].text
+        end = timezone[1].text
+        print(f"時間：{start}~{end}")
+        print("-----------------------")
+        driver.execute_script("window.history.go(-1)")
+        time.sleep(2)
+    next_page = driver.find_elements(By.XPATH,"/html/body/div[3]/div[2]/section[1]/div/div[2]/ul/li")
+    try:
+        if next_page[-1].text != ',':
+            print(f"共{next_page[-1].text}頁")
+            break
+        next_page[-1].click()
+    except IndexError:
+        break

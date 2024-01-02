4b2a834704cf8b2a8eef3dc07f2a56316344e7ad
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2024-01-02 17:15:35 +0800

    fix style

--- judge/190101.py
+++ judge/190101.py
@@ -1,19 +1,15 @@
 """
 ASCII sum
+
 描述
-
 如同我們剛才所說的，ASCII code是用來代表字元的一種編碼方式，可以透過ord(character)以及chr(code)來進行轉換：
-
 給定兩個輸入的字元，請將其轉換成 ASCII code 並且相加以後輸出。
-
 
 輸入
 輸入有兩行，各包含一個字元（character）
 
-
 輸出
 輸出有一行，包含一個整數
-
 
 輸入範例 1 
 a
--- judge/190301.py
+++ judge/190301.py
@@ -1,7 +1,7 @@
 """
 簡易計算機 II
-描述
 
+描述
 請實作一個函式calculator，依據輸入的運算符號做相對應得運算。
 
 def calculator(num1, num2, op):
@@ -9,17 +9,14 @@ def calculator(num1, num2, op):
     return ans
 PS 本題只需實作函式，並不用自行處理 input()
 
-
 輸入
 函式有三個參數：
-
 num1: 型態為 float
 num2: 型態為 float
 op: 型態為 str，代表要做的運算（本題支援"+", "-", "*", "/" 四種運算）。
 
 輸出
 函式有一個回傳值，型態為 float。
-
 
 輸入範例 1
 calculator(123, 234, "+")

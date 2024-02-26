5c6d665d1a94d694d4061c5e46c471d009122fa9
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2024-02-27 03:10:05 +0800

    resolve the dp problem

--- judge/214501.py
+++ judge/214501.py
@@ -26,3 +26,31 @@
 提示
 小明不能斜著走
 """
+
+# C(x+y−2,x−1)= (x+y−2)! // (x−1)!×(y−1)!
+
+# Solution 1
+# from math import factorial
+# x, y = [int(_) for _ in input().split()]
+# print(factorial(x + y - 2) // (factorial(x - 1) * factorial(y - 1)))
+
+# Solution 2
+def factorial(num):
+    if num == 0:
+        return 1
+    return num * factorial(num-1)
+
+def combination(n, k):
+    return factorial(n + k - 2) // (factorial(n - 1) * factorial(k - 1))
+
+x, y = [int(_) for _ in input().split()]
+print(combination(x, y))
+
+# Error：It will encounter a RecursionError
+# def factorial(number):
+#     if number == 0:
+#         return 1
+#     return number * factorial(number)
+
+# x, y = [int(_) for _ in input().split()]
+# print(factorial(x + y - 2) // (factorial(x - 1) * factorial(y - 1)))

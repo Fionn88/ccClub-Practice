4167496e642289d5bf675ce06823514d3dd37ebc
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2024-03-12 01:10:10 +0800

    solve the problem

--- judge/212302.py
+++ judge/212302.py
@@ -45,3 +45,39 @@ False
 提示
 Sample Input 1 就是上面示意圖的輸入喔！
 """
+
+def isValidSudoku(board):
+        # Check for duplicate numbers in the vertical
+        for row in board:
+            dup = {x for x in row if row.count(x) > 1}
+            if '.' in dup:
+                dup.remove('.')
+            if dup:
+                return False 
+
+        # Check for duplicate numbers in the horizontal
+        for col in range(9):
+            column = [board[row][col] for row in range(9)]
+            dup = {x for x in column if column.count(x) > 1}
+            if '.' in dup:
+                dup.remove('.')
+            if dup:
+                return False 
+
+        # Check for duplicate numbers in the 3x3 grid
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                dup = {x for x in square if square.count(x) > 1}
+                if '.' in dup:
+                    dup.remove('.')
+                if dup:
+                    return False 
+        return True
+
+lst = []
+for _ in range(9):
+    lst_inner = input().replace("0",".").split(",")
+    lst.append(lst_inner)
+
+print(isValidSudoku(lst))
